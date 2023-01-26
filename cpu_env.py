601532d8d52cccd38b714ba9137efb8720d03bd2
Mehrad <mehrad_milanloo@yahoo.com>
2023-01-26 19:45:14 +0330

    feat: dispatcher random selection  added

--- cpu_env.py
+++ cpu_env.py
@@ -10,21 +10,50 @@ class CPU(simpy.Resource):
 
 class Task:
     pid = 0
+    all_tasks = []
 
     class Priority(enum.Enum):
         LOW = 0
         NORMAL = 1
         HIGH = 2
 
-    def __init__(self, arrival_time, service_time, priority) -> None:
+    def __init__(self, arrival_time, service_time, max_timeout, priority) -> None:
         self.priority = Task.Priority(priority)
         self.service_time = service_time
         self.remaining_time = self.service_time
         self.arrival_time = arrival_time
+        self.max_timeout = max_timeout
         self.is_finished = False
         self.is_timeout = False
+        self.init_spend_times()
         self.pid = Task.pid
         Task.pid += 1
+        Task.all_tasks.append(self)
+    
+    def init_spend_times(self):
+        self.spend_times = {}
+        self.spend_times["Waiting_Queue"] = (0, 0)
+        for pq in Priority_Queue.Priority:
+            self.spend_times[pq.name] = (0, 0)
+
+    def update_queue_enter_time(self, queue_name, enter_time):
+        self.spend_times[queue_name] = (enter_time, self.get_spend_time(queue_name)[1])
+
+    def update_queue_exit_time(self, queue_name, exit_time):
+        self.spend_times[queue_name] = (self.get_spend_time(queue_name)[0], exit_time)
+    
+    def update_spend_times(self, queue_name, enter_time, exit_time):
+        self.spend_times[queue_name] = (enter_time, exit_time)
+
+    def get_spend_time(self, queue_name):
+        return self.spend_times[queue_name]
+
+    def get_total_spend_time(self):
+        total = 0
+        for pq in Priority_Queue.Priority:
+            total += self.spend_times[pq][1] - self.spend_times[pq][0]
+        return total
+
 
 class Priority_Queue:
 
@@ -38,13 +67,11 @@ class Priority_Queue:
         self.priority = Priority_Queue.Priority(priority)
         self.tasks = []
         self.Q_time = Q_time
-        # if Q_time == None:
-        #     self.Round_Robin = False
 
     def enqueue(self, task):
         self.tasks.append(task)
 
-    def dequeue(self):
+    def dequeue(self) -> Task:
         if self.tasks:
             return self.tasks.pop(0)
         else:
@@ -80,15 +107,16 @@ class Waiting_Queue:
     def length(self):
         return len(self.tasks)
 
-def job_creator(_lambda, _mu, priority_weights, waiting_queue: Priority_Queue, env: simpy.Environment):
+def job_creator(_lambda, _mu, Z, priority_weights, waiting_queue: Priority_Queue, env: simpy.Environment):
     while True:
-        # pending_time = expon.rvs(scale= (1/_lambda), size=1)
         priority = random.choices(([p.value for p in Task.Priority]), weights=priority_weights, k=1)[0]
         pending_time = int(random.expovariate(_lambda))
         yield env.timeout(pending_time)
         arrival_time = env.now
         service_time = int(random.expovariate(1/_mu))
-        task = Task(arrival_time, service_time, priority)
+        max_timeout = int(random.expovariate(1/Z))
+        task = Task(arrival_time, service_time, max_timeout, priority)
+        task.update_queue_enter_time(waiting_queue.name, env.now)
         waiting_queue.enqueue(task)
         print(str(env.now) + " " + "Task {} created".format(task.pid) + " Priority: " + str(task.priority) + " Arrival Time: " + str(task.arrival_time) + " Service Time: " + str(task.service_time))
 
@@ -102,35 +130,43 @@ def job_loader(sleep_time, K, waiting_queue: Waiting_Queue, queue_list, env: sim
             new_tasks = waiting_queue.dequeue(K)
             if new_tasks:
                 for task in new_tasks:
+                    task.update_queue_exit_time(waiting_queue.name, env.now)
                     queue_list[0].enqueue(task)
+                    task.update_queue_enter_time(queue_list[0].priority.name, env.now)
             print(str(env.now) + " " + "Job loading..." + " " + "Waiting Queue: " + str(waiting_queue.length()))
             for q in queue_list:
                 print(q.name + ": " + str(q.length()))
         yield env.timeout(sleep_time)
 
-def choose_queue(queue_list):
-    for q in queue_list:
-        if q.length() > 0:
-            return q
+def choose_queue(queue_list, weights) -> Priority_Queue:
+    q = random.choices(queue_list, weights=weights, k=1)[0]
+    if q.length() > 0:
+        return q
+    else:
         return None
 
-def dispatcher(env, cpu: CPU):
+    # for q in queue_list:
+    #     if q.length() > 0:
+    #         return q
+    #     return None
+
+def dispatcher(queue_choose_weights, cpu: CPU, env: simpy.Environment):
     while True:
         with cpu.request() as req:
             yield req
-            queue = choose_queue(cpu.queue_list)
+            queue = choose_queue(cpu.queue_list, queue_choose_weights)
             if queue:
+                print(str(env.now) + " " + "Dispatcher choose queue: " + queue.name)
                 task = queue.dequeue()
+                task.update_queue_exit_time(queue.priority.name, env.now)
                 print(str(env.now) + " " + "Task {} started".format(task.pid))
                 if queue.Q_time:
                     spend_time = min(task.remaining_time, queue.Q_time)
                     yield env.timeout(spend_time)
                     task.remaining_time -= spend_time
                     if task.remaining_time > 0:
-                        print(str(env.now) + " " + queue.name)
-                        print(len(cpu.queue_list))
-                        print(queue.priority.value)
                         cpu.queue_list[queue.priority.value + 1].enqueue(task)
+                        task.update_queue_enter_time(cpu.queue_list[queue.priority.value + 1].priority.name, env.now)
                     else:
                         task.is_finished = True
                         print(str(env.now) + " " + "Task {} finished".format(task.pid))
@@ -141,7 +177,4 @@ def dispatcher(env, cpu: CPU):
                     print(str(env.now) + " " + "Task {} finished".format(task.pid))
             else:
                 print(str(env.now) + " " + "CPU is idle")   
-        yield env.timeout(1) 
-    # env = simpy.Environment()
-    # env.process(job_creator(1, 2, [0.7, 0.2, 0.1], Priority_Queue(), env))
-    # env.run(until = 50)
+        yield env.timeout(1)
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import cpu_env
 import simpy
 
 env = simpy.Environment()
-cpu = cpu_env.CPU(cpu_env.Waiting_Queue("Priority_Queue", 1, 0.01), [cpu_env.Priority_Queue("Round_Robin_T1", 5, 0), cpu_env.Priority_Queue("Round_Robin_T2", 10, 1), cpu_env.Priority_Queue("FCFS", None, 2)], env)
-env.process(cpu_env.job_creator(0.3, 5, [0.7, 0.2, 0.1], cpu.waiting_queue, env))
+cpu = cpu_env.CPU(cpu_env.Waiting_Queue("Waiting_Queue", 1, 0.01), [cpu_env.Priority_Queue("Round_Robin_T1", 5, 0), cpu_env.Priority_Queue("Round_Robin_T2", 10, 1), cpu_env.Priority_Queue("FCFS", None, 2)], env)
+env.process(cpu_env.job_creator(0.3, 5, 0.3, [0.7, 0.2, 0.1], cpu.waiting_queue, env))
 env.process(cpu_env.job_loader(5, 5, cpu.waiting_queue, cpu.queue_list, env))
-env.process(cpu_env.dispatcher(env, cpu))
+env.process(cpu_env.dispatcher([0.8, 0.1, 0.1], cpu, env))
 env.run(until = 50)
